7a922c8a601f5446efa54c3bd76c1058cd42df83
PC <PC@DESKTOP-4QNGLLJ>
2025-04-20 03:37:19 +0200

    Part03:EmploeeModule-EditAction

--- Demo.PL/Controllers/DepartmentController.cs
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -136,6 +136,7 @@ namespace Demo.PL.Controllers
                 }
             }
             return View(viewModel);
+
         }
         #endregion
 
--- Demo.PL/Controllers/EmployeeController.cs
+++ Demo.PL/Controllers/EmployeeController.cs
@@ -2,12 +2,14 @@
 using Demo.BLL.DTO.EmploeesDtos;
 using Demo.BLL.Services.Classes;
 using Demo.BLL.Services.Intrfaces;
+using Demo.DAL.Models.EmployeeModel;
+using Demo.PL.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.PL.Controllers
 {
     public class EmployeeController (IEmployeeService _employeeService,
-                                      ILogger<DepartmentController> _logger,
+                                      ILogger<EmployeeController> _logger,
                                       IWebHostEnvironment _environment) : Controller
     {
         public IActionResult Index()
@@ -68,6 +70,65 @@ namespace Demo.PL.Controllers
             var employee = _employeeService.GetEmployeeById(id.Value);
             if(employee is null) return NotFound();  //404
             return View(employee);
+        }
+        #endregion
+
+        #region Edit Employee
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue) return BadRequest();  //400
+            var employee = _employeeService.GetEmployeeById(id.Value);
+            if (employee is null) return NotFound();  //404
+            var employeeDto = new UpdateEmployeeDto()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Salary = employee.Salary,
+                Address = employee.Address,
+                Age = employee.Age,
+                Email = employee.Email,
+                PhoneNumber = employee.PhoneNumber,
+                IsActive = employee.IsActive,
+                HiringDate = employee.HiringDate,
+                Gender = Enum.Parse<Gender>(employee.Gender) ,//To Convert String to Enum
+                EmployeeType = Enum.Parse<EmployeeType>(employee.EmployeeType) 
+            };
+            return View(employeeDto);
+        }
+        [ValidateAntiForgeryToken]  //id from Route  لازم
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int? id, UpdateEmployeeDto viewModel)
+        {
+
+            if (!ModelState.IsValid) return View(viewModel);
+            try
+            {
+                int result = _employeeService.UpdateEmployee(viewModel);
+                if (result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Employee Can't Be Updated !!");}
+            }
+            catch (Exception ex)
+            {
+
+                //log exception
+                if (_environment.IsDevelopment())
+                {
+                    //1.Development => log error in console And return same view with error msg
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                    //return View(departmentDto);
+                }
+                else
+                {
+                    //2.Deployment  => log error in file | table in database  And return Error view
+                    _logger.LogError(ex.Message);
+                    //return View(departmentDto);
+                }
+            }
+            return View(viewModel);
         }
         #endregion
     }
