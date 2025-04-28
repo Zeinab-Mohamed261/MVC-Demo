df3f9e46c49130e7c470ffe23040bd298dc008a2
PC <PC@DESKTOP-4QNGLLJ>
2025-04-28 12:36:52 +0300

    Part08:AuthLayout

---
+++ Demo.PL/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Register
+        public IActionResult Register()
+        {
+            return View();
+        } 
+        #endregion
+    }
+}
